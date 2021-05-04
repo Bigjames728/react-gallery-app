63cc314a19e1b98c446e90ce2fe8f1793fbcd351
Bigjames728 <bigjames728@gmail.com>
2021-05-04 15:17:27 -0700

    Started adding the code for Results component

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,2 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+export default class Results extends Component {
+  
+    render() {  
+        return (
+        
+            <div className="photo-container">
+                <h2>Results</h2>
+
+
+
+
+            </div>
+        );
+    }
+}
