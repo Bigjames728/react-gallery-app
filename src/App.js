692536bd3b1468fcda5697aed03ba4918b0b9162
Bigjames728 <bigjames728@gmail.com>
2021-05-04 16:50:41 -0700

    Made lots of different changes to code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import apiKey from './config';
 import './index.css';
+import axios from 'axios';
 import SearchForm from './components/SearchForm';
 import MainNav from './components/MainNav';
 import Results from './components/Results';
 
-function App() {
-  return (
-    <div>
-      
-      <SearchForm />
-      <MainNav />
-      <Results />
-        
-      
-    </div>
-  );
-}
+export default class App extends Component {
 
-export default App;
+  constructor() {
+    super();
+    this.state = {
+      images: [],
+      loading: true
+    };
+  }
+
+  
+
+  searchImages = (query) => {
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+      .then(response => {
+        this.setState({
+          images: response.photos.photo,
+          loading: false
+        })
+        console.log(response.photos.photo)
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
+  }
+
+
+  render() {
+    console.log(this.state)
+    return (
+      <div>
+        
+        <SearchForm onSearch={this.searchImages} />
+        <MainNav />
+        <Results />
+          
+        
+      </div>
+    );
+  }
+}
