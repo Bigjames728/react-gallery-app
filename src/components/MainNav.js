b3585e87e5dc2e9fffd20fb45192ee76185c6a91
Bigjames728 <bigjames728@gmail.com>
2021-05-04 15:14:00 -0700

    Created the MainNav component

---
+++ src/components/MainNav.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class MainNav extends Component {
+  
+    render() {  
+        return (
+        
+            <nav class="main-nav">
+                <ul>
+                <li><a href="#">Cats</a></li>
+                <li><a href="#">Dogs</a></li>
+                <li><a href="#">Computers</a></li>
+                </ul>
+            </nav>  
+        );
+    }
+}
