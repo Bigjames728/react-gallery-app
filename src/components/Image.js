2d2f35e3fab1cc9ef1b69f9be483daaf2e6db9e0
Bigjames728 <bigjames728@gmail.com>
2021-05-05 12:02:24 -0700

    Updated code in Image.js

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const Image = props => (
-    <li className="photo-container li">
-        <img alt=""/>
-    </li>
-);
+const Image = props => {
+    const image = props.data.photo;
+    const url = `https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`;
+    return (
+        <li className="photo-container li">
+            <img src={url} alt={image.title} />
+        </li>
+    );
+};
 
 export default Image;
