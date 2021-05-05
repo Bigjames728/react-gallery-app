1b60c5446963635b4a1289c90d20b59c895f9442
Bigjames728 <bigjames728@gmail.com>
2021-05-05 15:47:21 -0700

    Cleaned up some code

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -4,7 +4,7 @@ import Image from './Image';
 const ImageList = props => {
 
     const results = props.data;
-    console.log(results.photo)
+
     let images = results.map(image =>
         <Image data={image} key={image.id} />
     );
