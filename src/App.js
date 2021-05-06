966261a7bbe00f60ba01da5d7b7f2e1b228c6772
Bigjames728 <bigjames728@gmail.com>
2021-05-06 14:34:06 -0700

    Added dogs, cats and computer arrays to state and got the information and then stored it in the respective array

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import apiKey from './config';
 import './index.css';
-import {Route, BrowserRouter} from 'react-router-dom';
+import {Route, BrowserRouter, Redirect, Switch} from 'react-router-dom';
 import axios from 'axios';
 import SearchForm from './components/SearchForm';
 import MainNav from './components/MainNav';
@@ -14,6 +14,9 @@ export default class App extends Component {
     super();
     this.state = {
       images: [],
+      cats: [],
+      dogs: [],
+      computer: [],
       loading: true
     };
   }
@@ -21,8 +24,38 @@ export default class App extends Component {
   
 
   componentDidMount() { 
-    
+    // get 'cats' images and stores them in the cats array
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=cats&per_page=24&format=json&nojsoncallback=1`)
+      .then(response => {
+        this.setState({
+          cats: response.data.photos.photo
+        })
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
+    // get 'dogs' images and stores them in the dogs array
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=dogs&per_page=24&format=json&nojsoncallback=1`)
+      .then(response => {
+        this.setState({
+          dogs: response.data.photos.photo
+        })
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
+    // get 'computer' images and stores them in the computer array
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=computer&per_page=24&format=json&nojsoncallback=1`)
+      .then(response => {
+        this.setState({
+          computer: response.data.photos.photo
+        })
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
   }
+  
 
   performSearch = (query) => {
     axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${query}&per_page=24&format=json&nojsoncallback=1`)
