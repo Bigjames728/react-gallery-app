028f67825c04c660a770d8a8e69a795774260bef
Bigjames728 <bigjames728@gmail.com>
2021-05-10 16:28:35 -0700

    Added code comments

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+//A friendly not found page to display if the route doesn't exist or if there are no images returned for the search query.
+
 const NotFound = () => {
     return(
         <div className="not-found">
