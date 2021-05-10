ea544559bd998bb6c6e5bf084f668699574822d2
Bigjames728 <bigjames728@gmail.com>
2021-05-10 11:59:56 -0700

    Minor edits to text on NotFound.js component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,7 +3,8 @@ import React from 'react';
 const NotFound = () => {
     return(
         <div className="not-found">
-            <h2>Oops! It looks like this page doesn't exist.</h2>
+            <h2>Oops!</h2>
+            <p><strong>It looks like there are no images for this search term.</strong></p>
         </div>
     );
 };
