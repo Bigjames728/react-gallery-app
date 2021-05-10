21426f06ee88ac102fd1edb924d23c3cda47d0b4
Bigjames728 <bigjames728@gmail.com>
2021-05-10 16:33:11 -0700

    Added code comments

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+//This component includes the url I built that the image comes from. It takes the image by using the url and displays it in
+//the list element that will be inside the ul element.
+
 const Image = props => {
     const image = props.data;
     const url = `https://live.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`;
