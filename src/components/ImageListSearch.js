3cb95c7e2f0bb0a4732e1aa960eaee1e301f981b
Bigjames728 <bigjames728@gmail.com>
2021-05-10 16:27:33 -0700

    Added code comments

--- src/components/ImageListSearch.js
+++ src/components/ImageListSearch.js
@@ -3,6 +3,11 @@ import Image from './Image';
 import NotFound from './NotFound';
 import { withRouter } from 'react-router-dom';
 
+//I created this component strictly to help with the search query route. This component
+//helps by checking if the props.title matches the value entered into the search bar. If
+//not, it will re-load the images for the value in the search bar. This helped the refresh
+//button work appropriately. 
+
 const ImageListSearch = props => {
     
     const results = props.data;
