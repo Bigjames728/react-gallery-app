9504dde7e148404b7400976fe94b2ed992632c30
Bigjames728 <bigjames728@gmail.com>
2021-05-11 09:22:52 -0700

    Changed text in NotFound.js component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -6,7 +6,7 @@ const NotFound = () => {
     return(
         <div className="not-found">
             <h2>Oops!</h2>
-            <p><strong>It looks like there are no images for this search term.</strong></p>
+            <p><strong>It looks like this page doesn't exist..</strong></p>
         </div>
     );
 };
